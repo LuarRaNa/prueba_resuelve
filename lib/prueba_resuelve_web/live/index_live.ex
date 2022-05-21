c3a6f4ff4d03743e337ba916722dffa82297ce64
Luar RaNa <luar.rana@protonmail.com>
2022-05-21 03:19:58 -0500

    Interacción live_component - component
    - Detectar cambios en el input
    - Enviar json para mostrarlo en forma de tabla

--- lib/prueba_resuelve_web/live/index_live.ex
+++ lib/prueba_resuelve_web/live/index_live.ex
@@ -4,6 +4,15 @@ defmodule PruebaResuelveWeb.IndexLive do
     container: {:div, class: "h-5/6"}
 
   def mount(_params, _map, socket) do
+    socket = assign(socket, :input_json, %{})
     {:ok, socket}
+  end
+
+  def handle_event("new_json", %{"value" => value}, socket) do
+    with {:ok, json} <- Jason.decode(value) do
+      {:noreply, assign(socket, :input_json, json)}
+    else
+      {:error, _} -> {:noreply, assign(socket, :input_json, %{})}
+    end
   end
 end
