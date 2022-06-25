884987f6c6b57d817fdde005a38dde5efb85abf9
Luar RaNa <luar.rana@protonmail.com>
2022-06-24 21:03:43 -0500

    Actualizar formatter para incluir archivos heex
    - run mix format

--- lib/prueba_resuelve_web/live/json_live/to_table.ex
+++ lib/prueba_resuelve_web/live/json_live/to_table.ex
@@ -16,36 +16,36 @@ defmodule PruebaResuelveWeb.JsonLive.ToTable do
     ~H"""
     <% {head, body} = get_head_body(@json) %>
     <%= if map_size(@json) != 0 do %>
-    <div class="overflow-x-auto overflow-y-auto">
-      <table class="table table-compact w-full">
-        <thead>
-          <tr>
-            <th></th> 
-            <%= for th <- head do %>
-            <th> <%= th %> </th>
-            <% end %>
-          </tr>
-        </thead> 
-        <tbody>
-          <%= for {tr, counter} <- Enum.with_index(body) do %>
+      <div class="overflow-x-auto overflow-y-auto">
+        <table class="table table-compact w-full">
+          <thead>
             <tr>
-              <th> <%= counter %> </th>
+              <th></th>
               <%= for th <- head do %>
-              <td> <%= tr[th] %> </td>
+                <th><%= th %></th>
               <% end %>
             </tr>
-          <% end %>
-        </tbody>
-        <tfoot>
-          <tr>
-            <th></th> 
-            <%= for th <- head do %>
-            <th> <%= th %> </th>
+          </thead>
+          <tbody>
+            <%= for {tr, counter} <- Enum.with_index(body) do %>
+              <tr>
+                <th><%= counter %></th>
+                <%= for th <- head do %>
+                  <td><%= tr[th] %></td>
+                <% end %>
+              </tr>
             <% end %>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th></th>
+              <%= for th <- head do %>
+                <th><%= th %></th>
+              <% end %>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     <% end %>
     """
   end
--- test/prueba_resuelve_web/views/error_view_test.exs
+++ test/prueba_resuelve_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule PruebaResuelveWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(PruebaResuelveWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(PruebaResuelveWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
